70366c6ca77adac14a80164a705517a86700b8f6
David Foster <dlfosterii@yahoo.com>
2020-04-29 08:07:30 -0400

    HTML layout good

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const movieContainer = document.getElementById('movies-container')
+
+document.addEventListener(‘DOMContentLoaded’, function() {
+    // code here will execute after the document is loaded
+    function renderMovies(movieArray){
+        let movieHtmlArray = movieArray.map(function (currentMovie) {
+
+        }
+    }
+});
