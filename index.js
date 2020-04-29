075d0f2c39f7731ae18e7683b854367b88a53900
David Foster <dlfosterii@yahoo.com>
2020-04-29 15:45:38 -0400

    part 1 complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 
 const movieContainer = document.getElementById('movies-container')
+const myForm = document.getElementById('search-form');
 
-document.addEventListener(‘DOMContentLoaded’, function() {
-    // code here will execute after the document is loaded
-    function renderMovies(movieArray){
-        let movieHtmlArray = movieArray.map(function (currentMovie) {
-
-        }
+function renderMovies(movieArray){
+    let movieHtmlArray = movieArray.map(function (currentMovie) {
+        return `
+        <div id="movie" class="card m-3" style="width: 18rem;">
+        <img id="poster"
+            src="${currentMovie.Poster}"
+            class="card-img-top" alt="${currentMovie.Title}">
+        <div class="card-body">
+            <div class="titleDate">
+            <h5 id="title" class="card-title">${currentMovie.Title}</h5>
+            <h4 id="releaseDate"><span class="badge badge-secondary">${currentMovie.Year}</span></h4v>
+            </div>
+            <a href="#" class="btn btn-primary">Add!</a>
+        </div>
+        </div>`
     }
-});
+)
+return movieHtmlArray.join('');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // code here will execute after the document is loaded
+    myForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        movieContainer.innerHTML = renderMovies(movieData)
+        
+    });
+})
+
+
+
+
